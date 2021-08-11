383a3f1220b7f303015800b3141616ef758bfec2
kookcupid <kookcupid@gmail.com>
2021-08-11 08:51:42 +0700

    first version

---
+++ example.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kookcupid</title>
+    <style>
+        h1{
+            text-align: center;
+        }
+    </style>
+
+</head>
+<body>
+    <h1>Kookcupid</h1>
+    <?php
+        echo "<h1>KOOKCUPID</h1>";
+        echo "kook<br>";
+        echo "<b>Natthapon Auttama</b>";
+        print("kookcupid<br> print");
+    ?>
+
+    <form action="">
+        <label for="">ชื่อ</label>
+        <input type="text" name="" id="" value="<?php echo "กุ๊ก" ;?>">
+        <label for="">นามสกุล</label>
+        <input type="text" name="" id="" value="<?php echo "ไก้" ;?>">
+    </form>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+$name = "
+";
+$age = "34";
+echo "กุ๊ก =".$name."<br>";
+echo "อายุ=".$age."<br>";
+echo $age."<br>";
+
+$totle=1000;
+echo $totle."<br>";
+
+?>
