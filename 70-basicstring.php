f7f58f07e73994217e29cab6de082f914dcd54a7
kookcupid <88699187+kookcupid@users.noreply.github.com>
2021-08-12 09:13:37 +0700

    go

---
+++ 70-basicstring.php
@@ -0,0 +1,19 @@
+<?php
+
+$pass="kook123456789";
+
+echo "รหัส =".$pass."<br>";
+$result=md5($pass);
+echo "md5 =" .$result."<br>";
+
+echo "รหัส =".$pass."<br>";
+$result=sha1($pass);
+echo "sha1 =" .$result."<br>";
+
+echo "รหัส =".$pass."<br>";
+$result=sha1($pass);
+echo "sha1 =" .$result."<br>";
+
+
+
+?>
